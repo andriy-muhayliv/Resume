6d3b76204eda2a66e8efde3d8875869a10a3d0af
gyd3ukc <70544293+gyd3ukc@users.noreply.github.com>
2020-11-19 17:12:31 +0200

    Add files via upload

---
+++ js/java script.js
@@ -0,0 +1,9 @@
+function uaLang() {
+    document.querySelector("#aboutMe").innerHTML = "Про мене";
+    document.querySelector(".aboutMeDescription").innerHTML = "<p>Маю величезний досвід в <b>Adobe premier pro</b> і в <b>Adobe photoshop</b> поміняв багато різних робіт і не жалію про це, тому що ніколи не знаєш де та коли пригодить досвід який получив.</p><p>Досвід спілкування <b>англійською</b> вищий за середній та продовжую далі вивчати її та не тільки її, також вивчаю <b>JS</b> і моя наступна ціль <b>react!</b></p><p>Цю сторінку я збудував самостійно, надіюсь вона вам сподобалась.</p><h5>25 Років</h5>";
+}
+
+function engLang() {
+    document.querySelector("#aboutMe").innerHTML = "About me"
+    document.querySelector(".aboutMeDescription").innerHTML = "<p>Have a massive experience on <b>Adobe premier pro</b> and <b>Adobe photoshop</b> change a many different jobs and do not regret about it bacause you never know where and when this experience will come in handy.</p><p>Have a upper intermediate <b>english</b> spoken level, and continue learning every day and not only english also <b>JS</b> and my next target is <b>react</b>!</p><p>This page i byild myself, hope you like it.</p><h5>25 Y.O.</h5>";
+}
